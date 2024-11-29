685eac72fd98f8c70f3e97560fab2e0f7e0e9fde
btrizarchive <00001098434511sp@al.educacao.sp.gov.br>
2024-11-29 17:19:38 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Biologia',
+    'Quantas fases tem a mitose?',
+    '4 fases.'
+)
+
+criaCartao(
+    'biologia',
+    'Quais são as fases da mitose?',
+    'prófase,métafase,anáfase, telófase.'
+)
+
+criaCartao(
+    'Biologia',
+    'Diferença principal de eucariontes e procariontes?',
+    'A célula procariótica caracteriza-se pela ausência de núcleo e estrutura simples. Já a célula eucariótica tem núcleo definido e estrutura mais complexa.'
+)
+
+criaCartao(
+    'História',
+    'A guerra fria foi caracterizada por?',
+    'A polorização mundial, entre a disputa dos Eua com capitalismo e URSS com socialismo.'
+)
